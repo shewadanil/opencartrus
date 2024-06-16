ad4a534226dd456d134193db2603097d37ef8acd
shewadanil <shewa.danil@yandex.ru>
2024-06-16 16:50:16 +0300

    1

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿opencart-russia-language-4x
+Русский язык для Opencart 4x Русская сборка
+http://opencart-russia.ru
+
+ВНИМАНИЕ! ВНИМАНИЕ! 
+Данный перевод разрешается использовать только 
+в оригинальном OpenСart и в Русской сборке от opencart-russia.ru
+
+Спасибо за понимание!
---
+++ upload/catalog/language/ru-ru/account/subscription.php
@@ -0,0 +1,48 @@
+<?php
+// Heading
+$_['heading_title']                        = 'Подписки';
+
+// Text
+$_['text_account']                         = 'Аккаунт';
+
+$_['text_subscription']                    = 'Подписка';
+$_['text_subscription_id']                 = 'Идентификатор подписки';
+$_['text_subscription_detail']             = 'Реквизиты для оплаты подписки';
+$_['text_date_added']                      = 'Создан:';
+$_['text_status']                          = 'Статус:';
+$_['text_payment_method']                  = 'Способ оплаты:';
+$_['text_order_id']                        = '№ заказа:';
+$_['text_product']                         = 'Товар:';
+$_['text_quantity']                        = 'Количество:';
+$_['text_description']                     = 'Описание';
+$_['text_reference']                       = 'Связь';
+$_['text_transaction']                     = 'Транзакции';
+$_['text_transaction_date_added']          = 'Создан';
+$_['text_transaction_payment']             = 'Оплата';
+$_['text_transaction_outstanding_payment'] = 'Неуплаченный платеж';
+$_['text_transaction_skipped']             = 'Пропущенный платеж';
+$_['text_transaction_failed']              = 'Ошибка платежа';
+$_['text_transaction_cancelled']           = 'Отмененный';
+$_['text_transaction_suspended']           = 'Замороженный';
+$_['text_transaction_suspended_failed']    = 'Заморожен из за ошибки оплаты';
+$_['text_transaction_outstanding_failed']  = 'Неуплаченный из за ошибки оплаты';
+$_['text_transaction_expired']             = 'Истек';
+$_['text_no_results']                      = 'Периодические платежи не найдены';
+$_['text_error']                           = 'Периодические заказы не найдены!';
+$_['text_cancelled']                       = 'Периодический платеж был отменен';
+
+// Column
+$_['column_subscription_id']               = 'ID Профиля';
+$_['column_product']                       = 'Товар';
+$_['column_status']                        = 'Статус';
+$_['column_date_added']                    = 'Создан';
+$_['column_type']                          = 'Тип';
+$_['column_amount']                        = 'Итого';
+
+// Button
+$_['button_return']                        = 'Возврат';
+
+// Error
+$_['error_not_cancelled']                  = 'Ошибка: %s';
+$_['error_not_found']                      = 'Нельзя отключить профиль';
+
